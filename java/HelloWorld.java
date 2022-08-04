ae45ba605e02b872ff6921eccdbcd50190f38821
kenichi9999 <midukipapa1011@hotmail.com>
2022-08-04 16:33:19 +0900

    test

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld
     public static void main(String args[])
     {
         System.out.println("Hello Java World !");
-        System.out.println("java Git Push");
+        System.out.println("java Git Push & COMMIT");
     }
 }

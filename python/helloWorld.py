8117db65a967f48fe52bf09c6909ff6069da782d
kenichi9999 <midukipapa1011@hotmail.com>
2022-08-05 10:24:43 +0900

    python Study

--- python/helloWorld.py
+++ python/helloWorld.py
@@ -1,1 +1,25 @@
+
+# コメント
+
+x='123'
+print(x)
+
 print("hello world")
+
+
+
+#-----------------------------
+li = []
+li.append("python")
+print(li)
+li.append("php")
+print(li)
+
+
+#------------------------------
+
+for i  in range(10):
+     if i%2==0:
+         print("{} is even".format(i))
+     else:
+         print("{} is avan".format(i))

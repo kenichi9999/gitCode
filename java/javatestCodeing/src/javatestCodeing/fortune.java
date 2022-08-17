5c0d614145ddaea13fd34206370c1f733843854b
kenichi9999 <midukipapa1011@hotmail.com>
2022-08-17 10:59:40 +0900

    test

--- java/javatestCodeing/src/javatestCodeing/fortune.java
+++ java/javatestCodeing/src/javatestCodeing/fortune.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public  class fortune {
 	
 	static String[] result = {"大吉","中吉","小吉","吉"};
-	
+	/* fdsafdsafdsafdsafssd */
 	public static String getresult() {
 		Random rand = new Random();
 		return result[rand.nextInt(3)];
